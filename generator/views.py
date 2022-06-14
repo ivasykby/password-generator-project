019b58fd3db1710ffd69f3c49d7d8178dac62bde
Ivan B <ivasyk.by@gmail.com>
2022-06-14 10:28:03 +0300

    I made some changes

---
+++ generator/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+
+def passwords(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_+=/.,`~'))
+
+    length = int(request.GET.get('length', 12))
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/passwords.html', {'password': thepassword})
+
+
+def overview(request):
+    return render(request, 'generator/overview.html')
